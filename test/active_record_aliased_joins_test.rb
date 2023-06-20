9e9cc7065ad12e8ba0bc0ff3390e08fb09502f92
Larry Gebhardt <larry@cerebris.com>
2023-06-20 18:41:28 -0400

    Add support for left outer joins (#1)

--- test/active_record_aliased_joins_test.rb
+++ test/active_record_aliased_joins_test.rb
@@ -15,11 +15,29 @@ class ActiveRecordAliasedJoinsTest < Minitest::Test
                  sql
   end
 
+  def test_it_can_alias_has_many_outer
+    records = Post.left_joins_with_alias(:comments, :jr_post_comments)
+    sql = records.to_sql
+    assert_equal 'SELECT "posts".* FROM "posts" ' \
+                 'LEFT OUTER JOIN "comments" "jr_post_comments" ON "posts"."id" = "jr_post_comments"."post_id"',
+                 sql
+  end
+
   def test_it_can_alias_has_many_and_support_additional_join
     records = Post.joins_with_alias(:comments, :jr_post_comments).joins(author: :comments)
     sql = records.to_sql
     assert_equal 'SELECT "posts".* FROM "posts" ' \
                  'INNER JOIN "comments" "jr_post_comments" ON "posts"."id" = "jr_post_comments"."post_id" ' \
+                 'INNER JOIN "people" ON "people"."id" = "posts"."author_id" ' \
+                 'INNER JOIN "comments" ON "comments"."author_id" = "people"."id"',
+                 sql
+  end
+
+  def test_it_can_alias_has_many_and_support_additional_join_outer
+    records = Post.left_joins_with_alias(:comments, :jr_post_comments).joins(author: :comments)
+    sql = records.to_sql
+    assert_equal 'SELECT "posts".* FROM "posts" ' \
+                 'LEFT OUTER JOIN "comments" "jr_post_comments" ON "posts"."id" = "jr_post_comments"."post_id" ' \
                  'INNER JOIN "people" ON "people"."id" = "posts"."author_id" ' \
                  'INNER JOIN "comments" ON "comments"."author_id" = "people"."id"',
                  sql
@@ -35,6 +53,28 @@ class ActiveRecordAliasedJoinsTest < Minitest::Test
                  sql
   end
 
+  def test_it_can_alias_has_many_through_outer
+    records = Physician.left_joins_with_alias(:patients, :jr_patients)
+
+    sql = records.to_sql
+    assert_equal 'SELECT "physicians".* FROM "physicians" ' \
+                 'LEFT OUTER JOIN "appointments" ON "physicians"."id" = "appointments"."physician_id" ' \
+                 'LEFT OUTER JOIN "patients" "jr_patients" ON "appointments"."patient_id" = "jr_patients"."id"',
+                 sql
+  end
+
+  def test_it_can_alias_nested_has_many_through
+    skip
+    records = Physician.joins_with_alias(:towns, :jr_patient_towns)
+
+    sql = records.to_sql
+    assert_equal 'SELECT "physicians".* FROM "physicians" ' \
+                 'INNER JOIN "appointments" ON "physicians"."id" = "appointments"."physician_id" ' \
+                 'INNER JOIN "patients" ON "appointments"."patient_id" = "jr_patients"."id"' \
+                 'INNER JOIN "towns" "jr_patient_towns" ON "patients"."town_id" = "jr_patient_towns"."id"',
+                 sql
+  end
+
   def test_it_can_alias_belongs_to
     records = Post.joins_with_alias(:author, :jr_post_author)
     sql = records.to_sql
@@ -43,10 +83,26 @@ class ActiveRecordAliasedJoinsTest < Minitest::Test
                  sql
   end
 
+  def test_it_can_alias_belongs_to_outer
+    records = Post.left_joins_with_alias(:author, :jr_post_author)
+    sql = records.to_sql
+    assert_equal 'SELECT "posts".* FROM "posts" ' \
+                 'LEFT OUTER JOIN "people" "jr_post_author" ON "posts"."author_id" = "jr_post_author"."id"',
+                 sql
+  end
+
   def test_it_can_alias_has_one
     records = Person.joins_with_alias(:author_detail, :ad)
     sql = records.to_sql
     assert_equal 'SELECT "people".* FROM "people" INNER JOIN "author_details" "ad" ON "people"."id" = "ad"."person_id"',
+                 sql
+  end
+
+  def test_it_can_alias_has_one_outer
+    records = Person.left_joins_with_alias(:author_detail, :ad)
+    sql = records.to_sql
+    assert_equal 'SELECT "people".* FROM "people" ' \
+                 'LEFT OUTER JOIN "author_details" "ad" ON "people"."id" = "ad"."person_id"',
                  sql
   end
 
@@ -58,6 +114,17 @@ class ActiveRecordAliasedJoinsTest < Minitest::Test
     assert_equal 'SELECT "assemblies".* FROM "assemblies" ' \
                  'INNER JOIN "assemblies_parts" ON "assemblies"."id" = "assemblies_parts"."assembly_id" ' \
                  'INNER JOIN "parts" "jr_parts" ON "assemblies_parts"."part_id" = "jr_parts"."id"',
+                 sql
+  end
+
+  def test_it_can_alias_has_and_belongs_to_many_outer
+    records = Assembly.left_joins_with_alias(:parts, :jr_parts)
+
+    sql = records.to_sql
+
+    assert_equal 'SELECT "assemblies".* FROM "assemblies" ' \
+                 'LEFT OUTER JOIN "assemblies_parts" ON "assemblies"."id" = "assemblies_parts"."assembly_id" ' \
+                 'LEFT OUTER JOIN "parts" "jr_parts" ON "assemblies_parts"."part_id" = "jr_parts"."id"',
                  sql
   end
 
